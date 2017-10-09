ff459d566323e6d701e55dc18f10fdbe7206b7b9
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-09 14:16:20 -0500

    Inside the div append two button elements

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Code-Challenge-1
 [x] create a button on the DOM.
 [x] Use jquery to append a div
-[ ] Inside the div append two button elements
+[x] Paragraph counter
+[x] Inside the div append two button elements
 [ ] div should red background color
 [ ] Swap button should change parent background color from red to yellow
 [ ] Delete button should remove parent div container
--- script.js
+++ script.js
@@ -12,6 +12,6 @@ var button1= "<button type='button' id='button1' name='button1'>Swap</button>";
 var button2= "<button type='button' id='button2' name='button2'>Delete</button>";
   $('#generateD').append(append);
   $('#clickCount').html(clicks);
-  // $('#generateD').append(button1);
-  // $('#generateD').append(button2);
+  $('#generateD').append(button1);
+  $('#generateD').append(button2);
 }
