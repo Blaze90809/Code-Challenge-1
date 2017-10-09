8c80291fb904e1875e2f73cdb9c4c7f6b9104b69
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-09 14:34:10 -0500

    Swap button should change parent background color from red to yellow

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 [x] Use jquery to append a div
 [x] Paragraph counter
 [x] Inside the div append two button elements
-[ ] div should red background color
-[ ] Swap button should change parent background color from red to yellow
+[x] div should have a red background color
+[x] Swap button should change parent background color from red to yellow
 [ ] Delete button should remove parent div container
--- script.js
+++ script.js
@@ -2,16 +2,21 @@ $(document).ready(readyNow);
 
 function readyNow() {
   $('#gen').on('click', generateFunction);
+  $('.CodeClass').on('click', '#swapButton', swapFunction);
 }
 
 clicks=0;
 function generateFunction(){
   clicks++;
-var append = "<p>awesomeDiv</p>";
-var button1= "<button type='button' id='button1' name='button1'>Swap</button>";
+var append = "<div id='generateD'>awesomeDiv</div>";
+var button1= "<button type='button' id='swapButton' name='button1'>Swap</button>";
 var button2= "<button type='button' id='button2' name='button2'>Delete</button>";
-  $('#generateD').append(append);
-  $('#clickCount').html(clicks);
+  $('.CodeClass').append(append);
+  $('#generateD').html('<p>' + clicks + '</p>');
   $('#generateD').append(button1);
   $('#generateD').append(button2);
+}
+
+function swapFunction(){
+  $('#generateD').css('background-color', 'yellow');
 }
